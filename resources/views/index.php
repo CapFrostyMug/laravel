698617e4ed61cd31a273542e1046b020798571bf
Nick Yargin <nickolas7159@gmail.com>
2022-09-24 22:51:06 +0500

    Homework (lesson 1)
    Что нового:
    
    - создан и настроен новый проект;
    - добавлено несколько тестовых страниц (index, info, news).

---
+++ resources/views/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная</title>
+</head>
+<body style="display: flex; justify-content: center">
+<main style="justify-content: center">
+    <div style="width: 1366px; padding: 15px">
+        <h1>Добро пожаловать!</h1>
+    </div>
+    <div>
+        <ul style="list-style-type: none; display: flex">
+            <a href="/" style="margin-right: 15px"><li>Главная</li></a>
+            <a href="/info" style="margin-right: 15px"><li>О проекте</li></a>
+            <a href="/news" style="margin-right: 15px"><li>Новости</li></a>
+        </ul>
+    </div>
+</main>
+</body>
+</html>
